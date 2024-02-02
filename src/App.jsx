d417a416b9adf6c5b2f9ef3bd982c9de28c94f1e
jencruz21 <maxcruz4455@gmail.com>
2024-02-03 03:02:18 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+import "./assets/css/style.css";
+import NET from "vanta/src/vanta.net";
+import { ReactTyped } from "react-typed";
+
+function App() {
+  useEffect(() => {
+    NET({
+      el: "#app",
+      color: "#fff",
+      backgroundColor: "#03002E",
+      points: 9,
+      maxDistance: 20,
+      spacing: 15,
+      scale: 1,
+    });
+  }, []);
+
+  return (
+    <div className="app">
+      <div className="container" id="app">
+        <ReactTyped
+          strings={["Hello, I'm Jorgen Cruz", "New portfolio?"]}
+          typeSpeed={60}
+          backSpeed={60}
+          loop
+          showCursor={true}
+        >
+          <input className="header-title" type="text" />
+        </ReactTyped>
+      </div>
+    </div>
+  );
+}
+
+export default App;
